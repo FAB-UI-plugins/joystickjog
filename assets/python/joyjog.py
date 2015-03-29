c79c1c614fbaea5b9be4b5ebf7c0e1dde251f131
tohara <tom.haraldseid@gmail.com>
2015-03-29 15:00:49 +0800

    Working version. No GUI yet, other than a startbutton.

--- assets/python/joyjog.py
+++ assets/python/joyjog.py
@@ -1,27 +1,66 @@
-import time
-import sys, os
+import sys
+import time, datetime
 import serial
-from subprocess import call
-import numpy as np
 import json
-import pprint
+import ConfigParser
+import logging
 import ps3
 from time import sleep
-
 
 global feedrate
 
 global start_time
 
+config = ConfigParser.ConfigParser()
+config.read('/var/www/fabui/python/config.ini')
+
+macro_status = config.get('macro', 'status_file')
+
+#read config steps/units
+json_f = open(config.get('printer', 'settings_file'))
+units = json.load(json_f)
+#process params
 
 try:
-  
-    log_trace=str(sys.argv[1])    #trace log file
+    safety_door = int(units['safety']['door'])
+except KeyError:
+    safety_door = 0
 
+try:
+      
+    log_trace=str(sys.argv[1]) #param for the log file
+#     log_response=str(sys.argv[3]) #param for the log file
 except:
-    print "Missing Log reference"
-    sys.exit("Missing Log reference")
+    print("Missing params")
+    sys.exit()
+    
 
+#generic errors
+probe_start_time=0 #start time
+s_error=0
+s_warning=0
+s_skipped=0
+feeder_disengage_offset=units['feeder']['disengage-offset'] #mm distance to disable extruder
+
+trace_file=config.get('macro', 'trace_file')
+# response_file=config.get('macro', 'response_file')
+
+logging.basicConfig(filename=log_trace,level=logging.INFO,format='<span class="hidden-xs">[%(asctime)s] -</span> %(message)s',datefmt='%d/%m/%Y %H:%M:%S')
+
+
+
+
+open(trace_file, 'w').close() #reset trace file
+# open(response_file, 'w').close() #reset trace file
+
+def write_status(status):
+    global macro_status
+    json='{"type": "status", "status": ' + str(status).lower() +'}'
+    handle=open(macro_status,'w+')
+    print>>handle, json
+    return
+
+#track trace
 def trace(string):
     global log_trace
     out_file = open(log_trace,"a+")
@@ -32,7 +71,13 @@ def trace(string):
     return
 
 
-def macro1(code,expected_reply,timeout,error_msg,delay_after,warning=False,verbose=True):
+#get process pid so the UI can kill it if needed
+#myPID = os.getpid()
+#print myPID
+#trace(myPID,log_trace)
+#gcode macro exec
+
+def macro(code,expected_reply,timeout,error_msg,delay_after,warning=False,verbose=True):
     global s_error
     global s_warning
     global s_skipped
@@ -48,8 +93,13 @@ def macro1(code,expected_reply,timeout,error_msg,delay_after,warning=False,verbo
             #Expected reply
             #no reply:
             if (time.time()>=macro_start_time+timeout+5):
+    
                 if serial_reply=="":
                     serial_reply="<nothing>"
+                    
+                #trace_msg="failed macro (timeout):"+ code+ " expected "+ expected_reply+ ", received : "+ serial_reply
+                #trace(trace_msg,log_trace)
+                #print trace_msg
                 if not warning:
                     s_error+=1
                     trace(error_msg + ": Failed (" +serial_reply +")")
@@ -57,33 +107,21 @@ def macro1(code,expected_reply,timeout,error_msg,delay_after,warning=False,verbo
                     s_warning+=1
                     trace(error_msg + ": Warning! ")
                 return False #leave the function
+       
             serial_reply=serial.readline().rstrip()
+          
             #add safety timeout
             time.sleep(0.2) #no hammering
             pass
+       
         time.sleep(delay_after) #wait the desired amount
     else:
         trace(error_msg + ": Skipped")
         s_skipped+=1
         return False
+   
     return serial_reply
 
-
-trace("Joystick Jog initialized")
-try:
-    joystick = ps3.Ps3Com()
-except:
-    trace('Joystick not connected!')
-    sys.exit('Joystick not connected!')
-
-port = '/dev/ttyAMA0'
-baud = 115200
-try:
-    serial = serial.Serial(port, baud, timeout=0.6)
-    serial.flushInput()
-except:
-    trace('Serial failed')
-trace('Serial ok')
 def axisScale(val, span = 255, deadband = 20):
     tmp = val - (span/2.0)
     if (abs(tmp) < deadband):
@@ -103,34 +141,78 @@ def calculateGcode(jStatus):
     ySpeed = -axisScale(jStatus['LeftStickY'])
     zSpeed = axisScale(jStatus['RightStickY'])
     xyGain = 1.0
-    zGain = 1.0
+    zGain = 0.5
     feedRateGain = 5000.0
     
     gCode = 'G0 '
-    gCode += 'X%s ' % str(xSpeed * xyGain)
-    gCode += 'Y%s ' % str(ySpeed * xyGain)
-    gCode += 'Z%s ' % str(zSpeed * zGain)
-    gCode += 'F%s' % str(max((abs(xSpeed), abs(ySpeed), abs(zSpeed))) * feedRateGain )
+    gCode += 'X%0.2f ' % (xSpeed * xyGain)
+    gCode += 'Y%0.2f ' % (ySpeed * xyGain)
+    gCode += 'Z%0.2f ' % (zSpeed * zGain)
+    gCode += 'F%0.0f' % (max((abs(xSpeed), abs(ySpeed), abs(zSpeed))) * feedRateGain )
     
-    return gCode
+    move = max((abs(xSpeed), abs(ySpeed), abs(zSpeed))) > 0.0
     
+    return gCode, move
     
 
+write_status(True)
+'''#### SERIAL PORT COMMUNICATION ####'''
+serial_port = config.get('serial', 'port')
+serial_baud = config.get('serial', 'baud')
+
+serial = serial.Serial(serial_port, serial_baud, timeout=0.5)
+serial.flushInput()
 
 
-trace('before macro')
-reply = macro('G91', 'ok', 5, 'Set relative ', 1)  
-trace('Serial: ' & reply)
 
+reply = macro("G91","ok",1,"set relative movements",1)
+# trace('Serial: ' + reply)
+
+  
+js = ps3.Ps3Com()
 trace('starting')
+buttonSelectMem = False
+zProbeDown = False
 while 1:
-    status = joystick.getStatus()
+    status = js.getStatus()
+#     trace(status)
     if status['ButtonCircle']:
         trace('Joystick Jog aborted')
         break
-    gcode = calculateGcode(status)
-    trace(gcode)
-    macro(gcode, 'ok', 2, 'move', 0) 
+    if status['ButtonTriangle']:
+		macro("M999","ok",1,"reset",1)
+		
+
+		
+		
+    if status['ButtonSelect'] and (not buttonSelectMem):
+        buttonSelectMem = True
+        if not zProbeDown:
+            macro("M401","ok",1,"probe down",1)
+            zProbeDown = True
+        else:
+            macro("M402","ok",1,"probe up",1)
+            zProbeDown = False
+    elif (not status['ButtonSelect']) and buttonSelectMem:
+        buttonSelectMem = False
+		
+		
+    gCode, move = calculateGcode(status)
+#     trace(gCode, move)
+    if move:
+        #~ trace(gCode)
+        serial.write(gCode+"\r\n")
+        serial_reply=serial.readline().rstrip()
+        #~ trace('Serial: ' + serial_reply)
+        
+		
+
     
+#clean the buffer and leave
+serial.flush()
+serial.close()
+write_status(False)
+#open(trace_file, 'w').close() #reset trace file
+sys.exit()  
     
     
